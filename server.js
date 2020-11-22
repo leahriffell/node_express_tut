f1e7eebd6a7f78bc46dfb089a7cee18af2f6c054
Leah Riffell <leah.riffell@gmail.com>
2020-11-22 14:42:26 -0500

    Add post new paper endpoint

--- server.js
+++ server.js
@@ -38,3 +38,23 @@ app.get('/api/v1/footnotes', (request, response) => {
       response.status(500).json({ error });
     });
 });
+
+app.post('/api/v1/papers', (request, response) => {
+  const paper = request.body;
+
+  for (let requiredParameter of ['title', 'author']) {
+    if (!paper[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { title: <String>, author: <String> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+
+  database('papers').insert(paper, 'id')
+    .then(paper => {
+      response.status(201).json({ id: paper[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
